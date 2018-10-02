7996b9857f001b04ddc6cc09a31a95550daa160d
pedropmedina <pedrodelilia@gmail.com>
2018-10-01 23:39:17 -0400

    Added example asynchronicity with fetch and setTiemout to understand the microtask queue and callback queue working together

--- asynchronicity.js
+++ asynchronicity.js
@@ -1,4 +1,16 @@
 /**
+ * 8 parts in javascript engine:
+ * 1 - Global execution context
+ * 2 - Thread of execution
+ * 3 - Global Memory
+ * 4 - Call Stack
+ * 5 - Web browser features (Browser APIs)
+ * 6 - Event Loop
+ * 7 - Callback queue
+ * 8 - MicroTask queue
+ */
+
+/**
  * Synchronous:
  *
  * 1 - declare function display in global memory
@@ -74,12 +86,50 @@ blockFor1Sec();
 console.log('Me first');
 
 /**
- * The javascript engine has 7 main parts:
- * 1 - Global execution context
- * 2 - Thread of execution
- * 3 - Global Memory
- * 4 - Call Stack
- * 5 - Web browser features (Browser APIs)
- * 6 - Event Loop
- * 7 - Callback queue
+ * Bringing everything together in Asynchronicity:
+ * 1 - declare function display in memory and assign it its definition
+ * 2 - declare function printHello in memory and assign it its definition
+ * 3 - declare function blockFor300ms in memory and assign it its defintion
+ * 4 - spin up Timer feature in web browser APIs with printHello on 0ms
+ * 		sending printHello to the callback queue
+ * 5 - declare constant futureData in memory and assign it
+ * 		the returned promise object from calling fetch('https://twitter.com/1')
+ * 		where the 'value' property in the object is undefined at first
+ * 6 - pass to 'onFulfillment' property array in Promise object via 'then'
+ * 			the function definition 'display' to be triggered once value is set by					the response of xhr request.
+ * 7 - Once the xhr response is received, we push to 'display' function
+ * 			definition to the 'Microtask queue' (job queue) which handles modern
+ * 			web browser's features such as 'fetch'. The event loop continues
+ * 			to checek the call stack and prioritizes the Microtask queue over
+ * 			the callback queue (task queue)
+ * 7 - push to call stack 'blockFor300ms' which will ocupy the call stack
+ * 			for 300ms with long loop
+ * 8 - log to the console 'Me first'
+ * 9 - after the event loop checks the stack and ensures that is empty
+ * 		and the last line of synchronous code is executed, the engine
+ * 		pushes 'display' function defintion from the Microtask queue
+ * 		to the call stack
+ * 10 - log to the console the tweet from passed to display
+ * 11 - The event loop checks the call stack again and the Microtask queue
+ * 12 - Upon ensuring these two are empty, it, finally pushes 'printHello'
+ * 				function definition to the call stack
+ * 13 - log to the console 'Hello'
  */
+function display(data) {
+	console.log(data);
+}
+function printHello() {
+	console.log('Hello');
+}
+function blockFor300ms() {
+	/* blocks js thread for 300ms with long loop */
+}
+
+setTimeout(printHello, 0);
+
+const fuctureData = fetch('https://twitter.com/will/tweet/1');
+fuctureData.then(display);
+
+blockFor300ms();
+
+console.log('Me first');
