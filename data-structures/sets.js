808ad6c54078d2e70a0af460270f855c5d2d1841
pedropmedina <pedrodelilia@gmail.com>
2018-10-19 17:12:29 -0400

    Add intersection and difference methods to Set

--- data-structures/sets.js
+++ data-structures/sets.js
@@ -62,7 +62,8 @@ Set.prototype.values = function() {
 // returns a new set with unique elements from both sets.
 // Start by creating a new set and adding to it the elements of
 // set 1, then add to new set elements from set 2 that do not
-// already exists in the new set
+// already exists in the new set. This is pretty much
+// the default feature of sets
 Set.prototype.union = function(otherSet) {
 	let unionSet = new Set();
 
@@ -79,6 +80,42 @@ Set.prototype.union = function(otherSet) {
 	return unionSet;
 };
 
+// given both sets, return a set with elements that exists in both sets
+Set.prototype.intersection = function(otherSet) {
+	const intersectionSet = new Set();
+
+	const values = otherSet.values();
+	for (let i = 0; i < values.length; i++) {
+		if (this.has(values[i])) {
+			intersectionSet.add(values[i]);
+		}
+	}
+	return intersectionSet;
+};
+
+// given sets A and B, return set C with elements existing in A, and not in B,
+// and elements exisiting in B, and not in A
+Set.prototype.difference = function(otherSet) {
+	const differenceSet = new Set();
+
+	let values = this.values();
+	for (let i = 0; i < values.length; i++) {
+		if (!otherSet.has(values[i])) {
+			differenceSet.add(values[i]);
+		}
+	}
+
+	values = otherSet.values();
+	for (let i = 0; i < values.length; i++) {
+		if (!this.has(values[i])) {
+			differenceSet.add(values[i]);
+		}
+	}
+
+	return differenceSet;
+};
+
+// testing set
 const set = new Set();
 
 set.add(0);
@@ -104,7 +141,6 @@ console.log(values);
 console.log(set.items);
 
 // testing union
-
 const set1 = new Set();
 set1.add(1);
 set1.add(2);
@@ -117,3 +153,40 @@ set2.add(5);
 
 const unionSet = set1.union(set2);
 console.log(unionSet);
+
+// testing intersection
+const set3 = new Set();
+set3.add(1);
+set3.add(2);
+set3.add(3);
+set3.add(6);
+set3.add(7);
+set3.add(4);
+set3.add(5);
+
+const set4 = new Set();
+set4.add(2);
+set4.add(3);
+set4.add(5);
+set4.add(4);
+
+const intersection = set3.intersection(set4);
+console.log(intersection);
+
+// testing difference set
+const set5 = new Set();
+set5.add(1);
+set5.add(2);
+set5.add(3);
+set5.add(7);
+
+const set6 = new Set();
+set6.add(1);
+set6.add(3);
+set6.add(34);
+set6.add(320);
+set6.add(2);
+set6.add(5);
+
+const difference = set5.difference(set6);
+console.log(difference);
