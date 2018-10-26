8b0b2f73dcfd3a54f7c402d8c47f1c5f261dcb22
pedropmedina <pedrodelilia@gmail.com>
2018-10-25 23:13:57 -0400

    Add find, toArray, fromArray, toString to linkedList

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -6,12 +6,13 @@
 		4 - delete
 		5 - deleteHead
 		6 - deleteTail
-		7 - indexOf
-		8 - isEmpty
-		9 - size
-		10 - toArray
-		11 - fromArray
-		12 - toString
+		7 - find
+		8 - indexOf
+		9 - isEmpty
+		10 - size
+		11 - toArray
+		12 - fromArray
+		13 - toString
 */
 class Node {
 	constructor(value = null, next = null) {
@@ -177,6 +178,23 @@ class List {
 		return deleted;
 	}
 
+	find(value) {
+		if (!this.head) {
+			return null;
+		}
+
+		let current = this.head;
+
+		while (current) {
+			if (current.value === value) {
+				return current;
+			}
+			current = current.next;
+		}
+
+		return null;
+	}
+
 	indexOf(value) {
 		if (!value) {
 			return -1;
@@ -207,6 +225,27 @@ class List {
 
 	size() {
 		return this.length;
+	}
+
+	toArray() {
+		let current = this.head,
+			array = [];
+
+		while (current) {
+			array.push(current);
+			current = current.next;
+		}
+
+		return array;
+	}
+
+	fromArray(array) {
+		array.forEach(each => this.append(each));
+		return this;
+	}
+
+	toString(callback) {
+		return this.toArray().forEach(node => node.toString(callback));
 	}
 }
 
@@ -245,6 +284,16 @@ list.deleteHead();
 // list.deleteTail();
 
 list.delete('philippe');
+
+const found = list.find('bianca');
+console.log(found);
+
+const array = list.toArray();
+console.log(array);
+
+list.fromArray(['Lilia', 'Gricel']);
+
+list.toString(x => console.log('hello', x));
 
 console.log(list.head);
 console.log(list.tail);
